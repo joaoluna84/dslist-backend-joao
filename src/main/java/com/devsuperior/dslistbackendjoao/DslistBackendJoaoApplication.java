5d7b82f5e7d3956f82ce142eda6a075c5699d6e4
Joao Paulo Figner de Luna <joao.luna@bbts.com.br>
2023-07-17 00:36:55 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslistbackendjoao/DslistBackendJoaoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistbackendjoao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackendJoaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackendJoaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslistbackendjoao/DslistBackendJoaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistbackendjoao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendJoaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
